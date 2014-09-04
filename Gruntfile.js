50fd0ef0b96f05a58c77f1a7325531333f2776ff
Chris Morris <morris.w.chris@gmail.com>
2014-09-03 21:10:34 -0400

    converted crunchie to nodejs api wrapper for the time being. will abstract out the request call later to support ajax

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,6 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		clean: ["test/**"],
-		connect: {
-			test: {
-				options: {
-					middleware: function(connect) {
-						return [
-							connect.static('./test')
-						];
-					},
-					hostname: "0.0.0.0"
-				},
-			}
-		},
 		copy: {
 			test: {
 				files: [{
@@ -21,50 +9,16 @@ module.exports = function(grunt) {
 				}, {
 					src: "crunchie-test.js",
 					dest: "test/"
-				}, {
-					src: "test.html",
-					dest: "test/test.html"
-				}, {
-					src: "node_modules/jquery/dist/jquery.min.js",
-					dest: "test/jquery.js"
-				}, {
-					src: "node_modules/qunitjs/qunit/qunit.js",
-					dest: "test/qunit.js"
-				}, {
-					src: "node_modules/qunitjs/qunit/qunit.css",
-					dest: "test/qunit.css"
-				}, {
-					src: "node_modules/underscore/underscore.js",
-					dest: "test/underscore.js"
 				}]
 			}
 		},
-		qunit: {
-			all: {
-				options: {
-					urls: ["http://localhost:8000/test.html"]
-				}
-			}
-		},
-		watch: {
-			options: {
-				livereload: true
-			},
-			copy: {
-				files: ["crunchie*", "test.hmtl"],
-				tasks: "copy"
-			},
-			qunit: {
-				files: ["test/**/*"],
-				tasks: "qunit"
-			}
+		nodeunit: {
+			all: ["test/*-test.js"]
 		}
 	});
 	grunt.loadNpmTasks("grunt-contrib-clean");
 	grunt.loadNpmTasks("grunt-contrib-copy");
-	grunt.loadNpmTasks("grunt-contrib-connect");
-	grunt.loadNpmTasks("grunt-contrib-qunit");
-	grunt.loadNpmTasks("grunt-contrib-watch");
-	grunt.registerTask("test", ["clean", "copy", "connect", "qunit", "watch"]);
+	grunt.loadNpmTasks("grunt-contrib-nodeunit");
+	grunt.registerTask("test", ["clean", "copy", "nodeunit"]);
 	grunt.registerTask("default", ["test"]);
 };
--- crunchie-test.js
+++ crunchie-test.js
@@ -1,30 +1,58 @@
-(function($) {
-	//tests
-	var crunchie;
-	module("Crunchie", {
-		setup: function() {
-			crunchie = new Crunchie("4709f074cab6bd6c9ca6be1afa438b6f");
-		}
-	});
-	asyncTest("getOrganization", function() {
+var grunt = require("grunt"),
+	Crunchie = require("../crunchie"),
+	_ = require("underscore"),
+	crunchie = new Crunchie("4709f074cab6bd6c9ca6be1afa438b6f", true);
+
+module.exports = {
+	organizations: function(test) {
+		crunchie.getOrganizations(function(data) {
+			test.equal(_.isEmpty(data), false, "test getting organizations");
+			test.done();
+		});
+	},
+	organization: function(test) {
 		var expected = "0bf726bf19021756ca9ea4c8cd6a90a4";
 		crunchie.getOrganization("ziprecruiter", function(data) {
-			equal(expected, data.data.uuid, "test getting ziprecruiter");
-			start();
+			test.equal(expected, data.data.uuid, "test getting ziprecruiter");
+			test.done();
 		});
-	});
-	asyncTest("getPerson", function() {
+	},
+	people: function(test) {
+		crunchie.getPeople(function(data) {
+			test.equal(_.isEmpty(data), false, "test getting people");
+			test.done();
+		});
+	},
+	person: function(test) {
 		var expected = "d54ead02da35e0ca8f596b48eb3ba298";
 		crunchie.getPerson("john-david", function(data) {
-			equal(expected, data.data.uuid, "test getting john-david");
-			start();
+			test.equal(expected, data.data.uuid, "test getting john-david");
+			test.done();
 		});
-	});
-	asyncTest("getProduct", function() {
+	},
+	products: function(test) {
+		crunchie.getPeople(function(data) {
+			test.equal(_.isEmpty(data), false, "test getting products");
+			test.done();
+		});
+	},
+	product: function(test) {
 		var expected = "611af954cf18c35d01a2900209f1ce54";
 		crunchie.getProduct("cloudify", function(data) {
-			equal(expected, data.data.uuid, "test getting cloudify");
-			start();
+			test.equal(expected, data.data.uuid, "test getting cloudify");
+			test.done();
 		});
-	});
-})(jQuery);
+	},
+	locations: function(test) {
+		crunchie.getLocations(function(data) {
+			test.equal(_.isEmpty(data), false, "test getting locations");
+			test.done();
+		});
+	},
+	categories: function(test) {
+		crunchie.getLocations(function(data) {
+			test.equal(_.isEmpty(data), false, "test getting locations");
+			test.done();
+		});
+	},
+};
